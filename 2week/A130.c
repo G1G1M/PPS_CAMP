66c623d72a463f2d9a52e630d9ab6c25a4ee5428
G1G1M <kjw100404@gmail.com>
2024-07-09 00:00:33 +0900

    second commit

--- 2week/A130.c
+++ 2week/A130.c
@@ -1,36 +1,36 @@
 #include <stdio.h>
-#define SIZE 10000001
+#define SIZE 1000001
 
 int main()
 {
-  int K, N, sum = 0; // 줄 개수, 배열 크기 인덱스 값, 저장된 배열 값들의 합
+  int k, n, sum = 0; // 줄 개수, 배열 크기 인덱스 값, 저장된 배열 값들의 합
   int N1[SIZE];      // 정수를 저장할 배열
   int N2[SIZE];      // 새로운 정수를 저장할 배열
 
-  scanf("%d", &K);
+  scanf("%d", &k);
 
-  for (int i = 0; i < K; i++)
+  for (int i = 0; i < k; i++)
   {
     scanf("%d", &N1[i]);
   }
 
-  for (int j = 0; j < K; j++)
+  for (int i = 0; i < k; i++)
   {
-    if (N1[j] != 0)
+    if (N1[i] != 0)
     {
-      N2[N] = N1[j];
-      N++;
+      N2[n] = N1[i];
+      n++;
     }
     else
     {
-      N2[N - 1] = 0;
-      N--;
+      N2[n - 1] = 0;
+      n--;
     }
   }
 
-  for (int k = 0; k <= N; k++)
+  for (int i = 0; i <= n; i++)
   {
-    sum += N2[k];
+    sum += N2[i];
   }
 
   printf("%d\n", sum);
